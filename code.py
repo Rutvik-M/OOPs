775d88f8001755d4b7cd47674dc8a51f4cca17ad
Rutvik Manthalkar <manthalkar.rutvik@gmail.com>
2026-05-13 22:23:11 +0530

    fibo,prime,factorial added

--- code.py
+++ code.py
@@ -16,20 +16,60 @@
 # print (r)
 
 ## Palindrome 
-char = "madama"
-print (char[::-1])
+# char = "madama"
+# print (char[::-1])
 
-strs=list(char)
+# strs=list(char)
 
-def palindrome(char):
-    left=0
-    right=len(strs)-1
-    while left<right:
-        if strs[left] != strs[right]:
-            return ("false")
-        left+=1
-        right-=1
-        return ("true")
-s1=palindrome(strs)
-print(s1)
-        
+# def palindrome(char):
+#     left=0
+#     right=len(strs)-1
+#     while left<right:
+#         if strs[left] != strs[right]:
+#             return ("false")
+#         left+=1
+#         right-=1
+#         return ("true")
+# s1=palindrome(strs)
+# print(s1)
+
+# char = "madam"
+# text = list(char)
+# def pal(text):
+#     return text == text[::-1]
+
+# pal=pal("madama")
+# print(pal)
+
+
+## prime nUmber
+# n = 10
+# for i in range(2,n):
+#     if n%i==0:
+#         print("False")
+#         break
+#     else:
+#         print("True") 
+
+## factorial 5*4*3*2*1
+# num = 0
+# def factorial(num):
+#     fact=1
+#     if num<=0:
+#         return "not defined"
+
+    
+#     for i in range(1,num+1):
+#         fact=fact*i 
+#     return fact 
+# f1=factorial(num)
+# print(f1)
+
+## fibonanci num=7
+
+# def fibo(n):
+#     a,b=0,1
+#     for i in range(n):
+#         print(a,end=" ")
+#         a,b=b,a+b
+# fibo(5)
