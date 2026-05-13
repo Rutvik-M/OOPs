9a12c3f85364ea83cd09d12fe89f0c272e850cd9
Rutvik Manthalkar <manthalkar.rutvik@gmail.com>
2026-05-13 21:50:35 +0530

    optimal added

--- code.py
+++ code.py
@@ -1,4 +1,17 @@
 #reverse String 
-
 str="hello"
 print (str[::-1])
+chars = list(str)
+
+def reverse(chars):
+    left=0
+    right=len(chars)-1
+    while left< right:
+        chars[left],chars[right]=chars[right],chars[left]
+        left+=1
+        right-=1
+    return "".join(chars)
+  
+
+r=reverse(chars)
+print (r)
