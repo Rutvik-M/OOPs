17da7e63d5ec866713e8da9238231a755ecea21f
Rutvik Manthalkar <manthalkar.rutvik@gmail.com>
2026-05-13 15:22:30 +0530

    optimized

--- classes.py
+++ classes.py
@@ -3,10 +3,10 @@ class Student:
     age=0
     gender=""
 
-    def get_info(self):
-        self.name=input("Enter your name: ")
-        self.age=int(input("Enter your age: "))
-        self.gender = input("Enter your gender: ")
+    def get_info(self,name ,age,gender):
+        self.name=name
+        self.age=age
+        self.gender =gender
     
     def display(self):
         print(f"My name is {self.name} , Age is {self.age} , and Gender is {self.gender}")
